ab7a417b3187c7982945fa3293426fc40db3cd9c
jecktor <jecktor@proton.me>
2023-11-19 16:51:08 -0600

    Added tmux as default_prog in wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,19 @@
+local wezterm = require("wezterm")
+
+local tmux = {}
+
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	tmux = { "/opt/homebrew/bin/tmux", "new", "-As0" }
+else
+	tmux = { "tmux", "new", "-As0" }
+end
+
 return {
 	color_scheme = "zenbones_dark",
 
-	font = require("wezterm").font("JetBrainsMono Nerd Font"),
+	default_prog = tmux,
+
+	font = wezterm.font("JetBrainsMono Nerd Font"),
 	font_size = 13.0,
 
 	default_cursor_style = "SteadyBlock",
